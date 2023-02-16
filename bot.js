22b61968675b3466edf37b9a85480db18d2ccf5c
denikryt <56690968+denikryt@users.noreply.github.com>
2023-02-16 16:10:02 +0200

    heroku

--- bot.js
+++ bot.js
@@ -14,17 +14,17 @@ const botOptions = {
 
 class Bot {
   constructor() {
-    this.url = "https://denikryt-music-library.netlify.app";
+    // this.url = "https://denikryt-music-library.netlify.app";
     this.token = "5301226184:AAFeEf2yfBMjjBFEZgVEk9eSFQP4VilACUY";
     this.bot = new TelegramBot(this.token, botOptions);
     console.log("BOT", this.bot);
   }
 
   async start() {
-    // this.url = await ngrok.connect({
-    //   addr: process.env.PORT || 5000,
-    //   authtoken: "2AQMsLazUCSbrCi8ATjJWZMF5Wm_7haomK46oGei5EeBWb1wm", // if you have an ngrok account and want to use custom subdomains, provide your ngrok auth token here
-    // });
+    this.url = await ngrok.connect({
+      addr: process.env.PORT || 5000,
+      authtoken: "2AQMsLazUCSbrCi8ATjJWZMF5Wm_7haomK46oGei5EeBWb1wm", // if you have an ngrok account and want to use custom subdomains, provide your ngrok auth token here
+    });
     await this.bot.setWebHook(`${this.url}/bot${this.token}`);
     console.log(`Webhook set up at ${this.url}`);
   }
